8951b70ab473611b771588d130ce95fabeb24a8e
g0821009 <g0821009@gmail.com>
2015-06-26 10:53:39 +0900

    hogehoge

--- program_copy/Offset.vb
+++ program_copy/Offset.vb
@@ -1,6 +1,12 @@
-﻿Public Class Offset
+﻿Imports System.Drawing
+
+Public Class Offset
     Private textFile As String
-    Public Sub loadFile(ByVal arg As String)
+    Private deleteflag As Boolean = True
+
+    Public Sub loadFile(ByVal arg As String, ByVal fontsize As String)
+
+        TextBox1.Font = New Font("MS UI Gothic", fontsize)
 
         '読み込むテキストファイル
         textFile = arg
@@ -8,7 +14,7 @@
         Dim enc As System.Text.Encoding = System.Text.Encoding.GetEncoding("shift_jis")
 
         'テキストファイルの中身をすべて読み込む
-        Console.WriteLine(System.IO.File.ReadAllText(textFile, enc))
+        'Console.WriteLine(System.IO.File.ReadAllText(textFile, enc))
         TextBox1.Text = System.IO.File.ReadAllText(textFile, enc)
         Me.Text = System.IO.Path.GetFileName(arg)
     End Sub
@@ -20,18 +26,38 @@
 
         'ファイルが存在しているときは、上書きする
         System.IO.File.WriteAllText(textFile, TextBox1.Text, enc)
+        deleteflag = False
 
         Me.Close()
     End Sub
 
-    Private Sub Offset_KeyDown(sender As Object, e As Windows.Forms.KeyEventArgs) Handles Me.KeyDown
-
+    Private Sub Offset_Disposed(sender As Object, e As EventArgs) Handles Me.Disposed
+        If deleteflag Then
+            System.IO.File.Delete(textFile)
+        End If
+        Module1.fontsize = TextBox1.Font.Size
     End Sub
 
     Private Sub Offset_Load(sender As Object, e As EventArgs) Handles Me.Load
         'フォームの境界線スタイルを「None」にする
         'Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        Me.WindowState = Windows.Forms.FormWindowState.Maximized
         Me.TopMost = True
         Me.Activate()
     End Sub
+
+    '文字サイズダウン
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Font.Size > 10 Then
+            TextBox1.Font = New Font("MS UI Gothic", TextBox1.Font.Size - 4)
+        End If
+    End Sub
+
+    '文字サイズアップ
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Font.Size < 70 Then
+            TextBox1.Font = New Font("MS UI Gothic", TextBox1.Font.Size + 4)
+        End If
+    End Sub
+
 End Class
